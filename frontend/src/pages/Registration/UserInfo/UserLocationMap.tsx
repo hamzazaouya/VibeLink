ceacc41061683aa6b6de80371b9c0c1e6e66d05f
Mr_Y <61616479+hamzazaouya@users.noreply.github.com>
2025-07-26 17:36:02 +0000

    add the user location by ip address

--- frontend/src/pages/Registration/UserInfo/UserLocationMap.tsx
+++ frontend/src/pages/Registration/UserInfo/UserLocationMap.tsx
@@ -4,9 +4,20 @@ import { UserFormProps } from '../types/registration.types';
 import 'leaflet/dist/leaflet.css';
 
 function UserLocationMap(props: UserFormProps) {
-
     const [position, setPosition] = useState<{ lat: number; lng: number }>({lat: props.latitude, lng: props.longitude});
     
+    const getVisitorIP = async () => {
+      try {
+        let response = await fetch('https://api.ipify.org');
+        const ipAdress = await response.text();
+        response = await fetch(`http://ip-api.com/json${ipAdress}`);
+        const geoInfo = await response.json();
+        props.updateFields({ latitude: geoInfo.lat, longitude: geoInfo.lon});
+      } catch (error) {
+        
+      }
+    }
+
     useEffect(() => {
     if (!navigator.permissions || !navigator.geolocation) {
       console.warn("Geolocation or Permissions API not supported");
@@ -18,7 +29,6 @@ function UserLocationMap(props: UserFormProps) {
         navigator.geolocation.getCurrentPosition(
           (pos) => {
             props.updateFields({ latitude: pos.coords.latitude, longitude: pos.coords.longitude});
-            console.log("===============> ", pos.coords.latitude, pos.coords.longitude)
             setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude });
           },
           (err) => {
@@ -26,6 +36,7 @@ function UserLocationMap(props: UserFormProps) {
           }
         );
       } else if (result.state === "denied") {
+        getVisitorIP();
         console.warn("User has denied location access.");
       }
 
@@ -36,17 +47,19 @@ function UserLocationMap(props: UserFormProps) {
     });
     }, []);
   return (
-    <MapContainer  center={[position.lat, position.lng]} zoom={13} style={{ height: "100%", width: "40%", borderRadius: "15px"}}>
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[position.lat, position.lng]}>
-        <Popup>
-          You are here.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      <MapContainer  center={[position.lat, position.lng]} zoom={13} style={{ height: "100%", width: "40%", borderRadius: "15px"}}>
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[position.lat, position.lng]}>
+          <Popup>
+            You are here.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
   );
 }
 
