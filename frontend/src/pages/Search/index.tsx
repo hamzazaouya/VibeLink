3fc08e3e72790dc78a41e5c9d61594f62902e0b8
Achraf Ahrach <achrafahrach44@gmail.com>
2025-08-04 15:33:43 +0100

    feat: add settings rout

--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -1,7 +1,7 @@
 export default function Search() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-twilight-gradient-start via-twilight-gradient-middle to-twilight-gradient-end px-8 pt-28">
-      <h1>Search Page</h1>
+    <div className="min-h-screen w-screen pt-24 bg-gradient-to-br from-twilight-gradient-start via-twilight-gradient-middle to-twilight-gradient-end flex items-center justify-center">
+      <h1 className="text-2xl font-bold mb-4">🔍 Search Page</h1>
     </div>
   );
 }
--- frontend/src/pages/Settings/index.tsx
+++ frontend/src/pages/Settings/index.tsx
@@ -1,3 +1,7 @@
 export default function Settings() {
-  return <h1 className="text-2xl font-bold p-4">Your Profile Page</h1>;
+  return (
+    <div className="min-h-screen w-screen pt-24 bg-gradient-to-br from-twilight-gradient-start via-twilight-gradient-middle to-twilight-gradient-end flex items-center justify-center">
+      <h1 className="text-2xl font-bold mb-4">⚙️ Settings Page</h1>
+    </div>
+  );
 }
